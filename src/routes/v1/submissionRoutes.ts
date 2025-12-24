21237d1b779a3c082691c2ac473edd4fd851a8c7
krishsingh10386@gmail.com <bt23eci047@iiit.ac.in>
2025-12-24 15:32:18 +0530

    Fix: validator name changes to ZodValidator

--- src/routes/v1/submissionRoutes.ts
+++ src/routes/v1/submissionRoutes.ts
@@ -2,7 +2,7 @@ import express from "express";
 const submissionRouter = express.Router();
 
 import { addSubmission } from "../../controllers/submissionController.js";
-import { validate } from "../../validators/createSubmissionValidators.js";
+import { validate } from "../../validators/zodValidator.js";
 import { CreateSubmissionZodSchema } from "../../dtos/createSubmissionDtos.js";
 
 submissionRouter.post("/", validate(CreateSubmissionZodSchema), addSubmission);
